59d0b2db5718f38bf132c4950a38b4fa880c8c1d
Andreas Pålsson <apals@kth.se>
2015-05-13 15:29:48 +0200

    Fix snake movement. Adds a trail behind the player, but it's probably not right do to it this way.

--- Assets/SnakeScript.cs
+++ Assets/SnakeScript.cs
@@ -6,31 +6,31 @@ using System.Linq;
 public class SnakeScript : MonoBehaviour {
 	// Current Movement Direction
 	// (by default it moves to the right)
-	Vector2 dir = Vector2.right;
+	private Vector2 dir = Vector2.right;
+	private float turnSpeed = 2f;
+	public GameObject snakeTrailPrefab;
 	
 	// Use this for initialization
 	void Start () {
 		// Move the Snake every 300ms
-		InvokeRepeating("Move", 0.3f, 0.3f); 
+		//InvokeRepeating("Move", 0.3f, 0.3f); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		if (Input.GetKey(KeyCode.RightArrow))
-			dir = Vector2.right;
-		else if (Input.GetKey(KeyCode.DownArrow))
-			dir = -Vector2.up;    // '-up' means 'down'
-		else if (Input.GetKey(KeyCode.LeftArrow))
-			dir = -Vector2.right; // '-right' means 'left'
-		else if (Input.GetKey(KeyCode.UpArrow))
-			dir = Vector2.up;
 
-
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.forward * -turnSpeed);
+		 else if(Input.GetKey(KeyCode.LeftArrow)) 
+			transform.Rotate(Vector3.forward * turnSpeed);
+		
+		Move ();
 	}
 	
 	void Move() {
 		// Move head into new direction
 		transform.Translate(dir);
+		Instantiate (snakeTrailPrefab, this.transform.position, this.transform.rotation);
+
 	}
 }
