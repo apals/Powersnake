14bf9e30e868fad100d986e2265eb2f545c6dc83
Andreas Pålsson <apals@kth.se>
2015-05-13 15:00:02 +0200

    Initial commit. Simple snake version

---
+++ Assets/FollowSnakeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowSnakeScript : MonoBehaviour {
+
+	public Transform snakeTransform;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.position.Set (snakeTransform.position.x, snakeTransform.position.y, 0);
+		int DistanceAway = 10;
+		/*this.transform.position = new Vector3(snakeTransform.position.x, 
+		                                                               snakeTransform.position.y, 
+		                                                               snakeTransform.position.z - DistanceAway);
+		                                                               */
+	}
+}
---
+++ Assets/SnakeScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SnakeScript : MonoBehaviour {
+	// Current Movement Direction
+	// (by default it moves to the right)
+	Vector2 dir = Vector2.right;
+	
+	// Use this for initialization
+	void Start () {
+		// Move the Snake every 300ms
+		InvokeRepeating("Move", 0.3f, 0.3f); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.RightArrow))
+			dir = Vector2.right;
+		else if (Input.GetKey(KeyCode.DownArrow))
+			dir = -Vector2.up;    // '-up' means 'down'
+		else if (Input.GetKey(KeyCode.LeftArrow))
+			dir = -Vector2.right; // '-right' means 'left'
+		else if (Input.GetKey(KeyCode.UpArrow))
+			dir = Vector2.up;
+
+
+	}
+	
+	void Move() {
+		// Move head into new direction
+		transform.Translate(dir);
+	}
+}
